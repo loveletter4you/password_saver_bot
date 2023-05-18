6b37770cb52860ffa43fab58c6bd7b886dfa411e
Valery Korolev <korolevvalery37@gmail.com>
2023-05-18 23:16:11 +0600

    tg_id_bugfix

--- bot/bot/models/service.py
+++ bot/bot/models/service.py
@@ -6,7 +6,7 @@ from bot.models.base import Base
 class Service(Base):
     __tablename__ = "service"
     id = Column(BigInteger, primary_key=True, index=True)
-    telegram_id = Column(Integer)
+    telegram_id = Column(BigInteger)
     service_name = Column(String)
     login = Column(String)
     password = Column(String)
